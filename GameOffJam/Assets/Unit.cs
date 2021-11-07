f1b5d7b3fb994f10a4fac4bba6a05516fb366fde
Frictionpowered <denes@nagymathe.com>
2021-11-07 12:00:58 +0000

    added a scene with a unit that uses navigation

---
+++ GameOffJam/Assets/Unit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+	public GameObject moveTarget;
+	public NavMeshAgent agent;
+
+	void Start ()
+	{
+		moveTarget = GameObject.Find("Base");
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	void Update ()
+	{
+		if (agent && moveTarget)
+		{
+			Vector3 targetPos = moveTarget.transform.position;
+			if (!agent.hasPath || agent.isPathStale
+				|| (agent.destination - targetPos).sqrMagnitude > 1.0f)
+			agent.SetDestination(targetPos);
+		}
+	}
+}
